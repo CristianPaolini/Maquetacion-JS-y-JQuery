5538902c4b5331732720adff2313433bf8a727fb
Cristian Paolini <cristianpaolini3@gmail.com>
2021-01-19 21:10:03 -0300

    Listado dinámico de post almacenado en json funcionando

--- js/main.js
+++ js/main.js
@@ -1,7 +1,56 @@
+
+// Slider
+
 $(document).ready(function(){
 $('.bxslider').bxSlider({
     mode: 'fade',
     captions: true,
-    slideWidth: 1200
+    slideWidth: 1200,
+    responsive: true,
+    pager: true
   });
+
+// Post
+var posts = [
+	{
+		title: 'Prueba de título 1',
+		date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+		content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		title: 'Prueba de título 2',
+		date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+		content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		title: 'Prueba de título 3',
+		date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+		content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		title: 'Prueba de título 4',
+		date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+		content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		title: 'Prueba de título 5',
+		date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+		content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+];
+
+	posts.forEach((item, index) => {
+		var post = `
+			<article class="post">
+				<h2>${item.title}</h2>
+				<span class="date">${item.date}</span>
+				<p>
+					${item.content}
+				</p>
+				<a href="#" class="button-more">Leer más</a>
+			</article>
+		`;
+
+		$("#posts").append(post);
+	});
 });
